cfb4ec1364a860c76fd7e1da97abcd2583a861a9
bylie320 <76068835+bylie320@users.noreply.github.com>
2023-09-15 23:11:18 +0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,3 @@
 
+--0user_gtm_go=start; sso_token_mttb=ycxjmpze7mogkks8swsk00kg; _gid=GA1.2.949601186.1694809076; ga_cid=53366009.1694809075; tmr_lvid=6346734a79b0487d91cfa855555c5e01; tmr_lvidTS=1694809076343; _ym_uid=1694809076823137939; _ym_d=1694809076; _ym_isad=2; _ym_visorc=w; _gat_UA-52521450-1=1; PHPSESSIDBSNS=rcc0h8sctoeuamb3kipf3bkcn1; user_gtm_session=start; user_gtm_go=start; HelloFirstTime=true; _gat=1; _ga_054895ELP6=GS1.1.1694809075.1.1.1694809123.12.0.0; _ga=GA1.2.53366009.1694809075; _ga_9EMGRBYQ05=GS1.1.1694809075.1.1.1694809124.0.0.0; tmr_detect=0%7C1694809126249--0/2671845||0/2671847-=-
+  
